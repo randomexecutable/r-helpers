75af9c24fae245bad7158fc72e4352ece8bf5ae4
Randomexecutable <randomexecutable@gmail.com>
2014-04-13 15:08:50 +0300

    Changed ColonyCoefficientNames into a one-liner and added a function for selection of best averages from the dilution data.

--- colony_helpers.R
+++ colony_helpers.R
@@ -1,17 +1,22 @@
+# Downloaded from:
 # https://github.com/randomexecutable
 # MIT license
 #
 
+# Searches for column names starting with C and followed by numbers
+# with dot, which is again followed by numbers
+# e.g. column name C3.2 would be found
+#
 ColonyCoefficientNames <- function(names) {
-  colony.coefs <- names[grep("C\\d+[.]\\d+", names)]
-  return(colony.coefs)
+  return(names[grep("C\\d+[.]\\d+", names)])
 }
 
 # Returns the number of technical replicates per dilution level
 # in a vector with dilution levels as names
 # e.g.
 # C4 C5  <- dilution levels (10^4, 10^5)
-# 4  4   <- number of technical replicates in the data file (C4.1-C4.4, C5.1-C5.4)
+# 4  4   <- number of technical replicates in the data 
+# file (C4.1-C4.4, C5.1-C5.4)
 #
 ColonyCoefficientTechrVector <- function(coefs) {
   res <- numeric()
@@ -26,4 +31,30 @@ ColonyCoefficientTechrVector <- function(coefs) {
     }
   }
   return(res)
+}
+
+# Calculates the average from technical replicates 
+# and selects the best candidate for for further
+# calculations
+#
+ColonySelectBestAvg <- function(data, coefs.vect) {
+  data['selected'] <- NA
+  for(i in seq_along(names(coefs.vect))) {
+    treplicates <- character(0)
+    dilution.label <- names(coefs.vect[i])
+    number.treplicates <- as.numeric(coefs.vect[dilution.label])
+    for(j in seq_along(1:number.treplicates)) {
+      current <- paste(names(coefs.vect)[i], as.character(j), sep=".")
+      treplicates <- append(treplicates, current)
+    }
+    total.sum <- rowSums(data[treplicates])
+    data <- cbind(data, (total.sum/number.treplicates))
+    names(data)[length(data)] <- dilution.label
+    index <- is.na(data[['selected']])
+    data[['selected']][index] <- dilution.label
+    tempcol <- which(colnames(data) == dilution.label)
+    index2 <- (data[[tempcol]] > 14 & data[[tempcol]] < 65)
+    data[['selected']][index2] <- dilution.label
+  }  
+  return(data)
 }
